60bf623dbf5ac107b3114dc46c8b2a695c129b41
Balzac <balzac.baudemont@gmail.com>
2024-05-27 17:09:50 +0200

    update modal style

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,7 +9,7 @@ interface Props {
 function Modal({ title, text, onClose }: Props) {
   return (
     <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md mx-2 text-black text-center">
+      <div className="bg-zinc-900 rounded-lg shadow-lg p-6 w-full max-w-md mx-2 text-center">
         <h1 className="text-xl font-semibold">{title}</h1>
         <p className="my-4">{text}</p>
         {onClose && <Button onClick={onClose} text="Close" />}
