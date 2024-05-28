7a6b49cd03faf6f678550b2c1cd7da6230b5174f
Balzac <balzac.baudemont@gmail.com>
2024-05-28 18:57:32 +0200

    update keys color when keyTestMode is enabled

--- src/views/scene/Keyboard.tsx
+++ src/views/scene/Keyboard.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { LoopOnce, Mesh, MeshStandardMaterial } from "three";
 import { RootState } from "../../reducers/store";
+import { keyActions } from "../../constants/keyActions";
 
 function Keyboard() {
   // TODO Find a way to correctly type the useRef for a R3F group
@@ -29,6 +30,17 @@ function Keyboard() {
     }
   };
 
+  const updateGroupNodeColor = (
+    target: "key" | "text" | "top" | "bottom",
+    color: string
+  ) => {
+    const nodesId = Object.keys(nodes).filter((key) =>
+      key.includes(`_${target}`)
+    );
+    const material = new MeshStandardMaterial({ color });
+    nodesId.forEach((id) => updateNodeColor(id, material));
+  };
+
   // Play a specific animation
   const playAnimation = (animation: string) => {
     actions[animation]?.setLoop(LoopOnce, 1);
@@ -38,17 +50,15 @@ function Keyboard() {
   // Key down listener
   useEffect(() => {
     const onKeyDown = (event: KeyboardEvent) => {
-      if (animations && animations.length) {
-        if (event.code === "Space") {
-          playAnimation("press_space_bar");
-          if (settings.keyTestMode) {
-            updateNodeColor(
-              "space_bar_key",
-              new MeshStandardMaterial({ color: "green" })
-            );
-          }
+      const action = keyActions[event.code];
+      if (action) {
+        playAnimation(action.animation);
+        if (settings.keyTestMode) {
+          updateNodeColor(
+            action.node,
+            new MeshStandardMaterial({ color: "green" })
+          );
         }
-        // TODO, do that for all keys...
       }
     };
 
@@ -63,13 +73,16 @@ function Keyboard() {
   useEffect(() => {
     Object.keys(colors).forEach((key) => {
       const target = key as keyof typeof colors;
-      const nodesId = Object.keys(nodes).filter((key) =>
-        key.includes(`_${colorMapping[target]}`)
-      );
-      const material = new MeshStandardMaterial({ color: colors[target] });
-      nodesId.forEach((id) => updateNodeColor(id, material));
+      updateGroupNodeColor(colorMapping[target], colors[target]);
     });
   }, [colors]);
+
+  // Reset keys color on turning off the key test mode
+  useEffect(() => {
+    if (settings.keyTestMode === false) {
+      updateGroupNodeColor("key", colors.keys);
+    }
+  }, [settings.keyTestMode]);
 
   return (
     <group ref={groupRef}>
