c8a3c2c0bb3d5118d31c3076a4fd7c661346d0c9
Balzac <balzac.baudemont@gmail.com>
2024-05-28 15:46:01 +0200

    update keyboard colors based on colors reducer

--- src/components/scene/Keyboard.tsx
+++ src/components/scene/Keyboard.tsx
@@ -1,12 +1,15 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useEffect, useRef } from "react";
-import { LoopOnce } from "three";
+import { useSelector } from "react-redux";
+import { LoopOnce, Mesh, MeshStandardMaterial } from "three";
+import { RootState } from "../../reducers/store";
 
 function Keyboard() {
   // TODO Find a way to correctly type the useRef for a R3F group
   const groupRef = useRef<any>();
-  const { scene, animations } = useGLTF("/models/keyboard.glb");
+  const { nodes, scene, animations } = useGLTF("/models/keyboard.glb");
   const { actions } = useAnimations(animations, groupRef);
+  const colors = useSelector((state: RootState) => state.colors);
 
   // Add a listener on keys to play animation
   useEffect(() => {
@@ -26,6 +29,39 @@ function Keyboard() {
       window.removeEventListener("keydown", onKeyDown);
     };
   }, []);
+
+  const updateColor = (
+    target: "key" | "text" | "top" | "bottom",
+    color: string
+  ) => {
+    const nodesId = Object.keys(nodes).filter((key) =>
+      key.includes(`_${target}`)
+    );
+    const material = new MeshStandardMaterial({ color });
+    nodesId.forEach((id) => {
+      const node = scene.getObjectByName(id) as Mesh | null;
+      if (node) {
+        node.material = material;
+      } else {
+        console.warn(`Node with id ${id} not found in the scene.`);
+      }
+    });
+  };
+
+  // Define the mapping between colors state properties and target keys
+  const colorMapping = {
+    keys: "key",
+    letters: "text",
+    caseTop: "top",
+    caseBottom: "bottom",
+  } as const;
+
+  useEffect(() => {
+    Object.keys(colors).forEach((key) => {
+      const target = key as keyof typeof colors;
+      updateColor(colorMapping[target], colors[target]);
+    });
+  }, [colors]);
 
   return (
     <group ref={groupRef}>
