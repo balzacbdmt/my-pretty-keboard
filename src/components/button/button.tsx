c7efa00a97999120f89e138aa3cac21aff89e2bd
Balzac <balzac.baudemont@gmail.com>
2024-05-31 10:40:26 +0200

    Update button component to receive an icon and a rounded prop

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,18 +1,29 @@
 import { MouseEventHandler } from "react";
+import { Icon } from "@iconify/react";
+import { join } from "../../constants/helpers";
 
 interface Props {
-  text: string;
+  text?: string;
+  icon?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   isDisabled?: boolean;
+  rounded?: boolean;
 }
 
-function Button({ text, onClick, isDisabled }: Props) {
+function Button({ text, icon, onClick, isDisabled, rounded }: Props) {
+
+  const className = join([
+    "w-fit bg-blue-500 enabled:hover:bg-blue-700 disabled:opacity-75 text-white font-bold py-2 px-4 rounded",
+    rounded ? "rounded-full	aspect-square py-2 px-2" : "",
+  ])
+
   return (
     <button
-      className="bg-blue-500 enabled:hover:bg-blue-700 disabled:opacity-75 text-white font-bold py-2 px-4 rounded"
+      className={className}
       onClick={onClick}
       disabled={isDisabled}
     >
+      {icon && <Icon color="white" fontSize={38} icon={icon} />}
       {text}
     </button>
   );
