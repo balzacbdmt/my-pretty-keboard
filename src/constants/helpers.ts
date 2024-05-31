b3f614df28d730db86cf4d3ebb41e1f0de5bd7e3
Balzac <balzac.baudemont@gmail.com>
2024-05-31 10:39:13 +0200

    create a join helper

---
+++ src/constants/helpers.ts
@@ -0,0 +1,9 @@
+/**
+ * Helper that joins strings together with a space
+ * @param items - Array of strings to be joined.
+ * @returns The joined string, with items separated by a space.
+ * @example
+ * join(["A", "B", "C"]);
+ * // returns "A B C"
+ */
+export const join = (items: string[]) => items.join(" ");
