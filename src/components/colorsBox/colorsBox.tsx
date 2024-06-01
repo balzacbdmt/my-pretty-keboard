cb2ca0f69b534518f0590c78286a1ec6bab149ba
Balzac <balzac.baudemont@gmail.com>
2024-06-01 10:24:11 +0200

    Create and integrate a Tooltip component

--- src/components/colorsBox/colorsBox.tsx
+++ src/components/colorsBox/colorsBox.tsx
@@ -1,28 +1,33 @@
 import { useSelector } from "react-redux";
 import { join } from "../../constants/helpers";
 import { RootState } from "../../reducers/store";
+import Tooltip from "../tooltip/Tooltip";
 
 function ColorsBox() {
   const colors = useSelector((state: RootState) => state.colors);
 
-  const colorClass = (index: number, length: number) =>
+  const colorClassName = (index: number, length: number) =>
     join([
-      "flex-1",
+      "w-full h-full",
       index === 0 ? "rounded-l-lg" : "",
       index === length - 1 ? "rounded-r-lg" : "",
     ]);
 
-  // TODO: add a tooltip on hovering a color
   // TODO: add to press-paper on click a color
 
   return (
     <div className="flex w-full h-10">
       {Object.values(colors).map((color, index) => (
-        <div
+        <Tooltip
+          className="flex-1"
+          content={color}
           key={index}
-          className={colorClass(index, Object.values(colors).length)}
-          style={{ backgroundColor: color }}
-        />
+        >
+          <div
+            className={colorClassName(index, Object.values(colors).length)}
+            style={{ backgroundColor: color }}
+          />
+        </Tooltip>
       ))}
     </div>
   );
---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,48 @@
+import { ReactNode, useState } from "react";
+import { join } from "../../constants/helpers";
+
+interface Props {
+  className?: string;
+  children: ReactNode;
+  content: ReactNode;
+  direction?: "top" | "right" | "bottom" | "left";
+}
+
+function Tooltip({ className, children, content, direction = "top" }: Props) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const getDirectionClasses = () => {
+    switch (direction) {
+      case "top":
+        return "bottom-full left-1/2 transform -translate-x-1/2 mb-1";
+      case "right":
+        return "top-1/2 left-full transform -translate-y-1/2 ml-1";
+      case "bottom":
+        return "top-full left-1/2 transform -translate-x-1/2 mt-1";
+      case "left":
+        return "top-1/2 right-full transform -translate-y-1/2 mr-1";
+      default:
+        return "";
+    }
+  };
+
+  const containerclassName = join(["relative", className || ""]);
+
+  const tooltipClassName = join([
+    "absolute z-10 bg-gray-800 text-white text-sm p-2 rounded opacity-90 whitespace-nowrap",
+    getDirectionClasses(),
+  ]);
+
+  return (
+    <div
+      className={containerclassName}
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+    >
+      {children}
+      {isVisible && <div className={tooltipClassName}>{content}</div>}
+    </div>
+  );
+}
+
+export default Tooltip;
