6b5623e7632315a44f205b65bdf65e6e489464ca
Balzac <balzac.baudemont@gmail.com>
2024-06-01 23:04:32 +0200

    add a copyToClipboard utils method

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -14,3 +14,18 @@ export const join = (items: string[]) => items.join(" ");
 export const generateRandomId = (): string => {
   return Math.random().toString(36).slice(2, 11);
 };
+
+/**
+ * Put specific text in the clipboard
+ * @param text string to be putted in the clipboard
+ * @returns boolean as true if successfully putted in the clipboard, false otherwise
+ */
+export const copyToClipboard = async (text: string): Promise<boolean> => {
+  try {
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch (err) {
+    console.error("Failed to copy text: ", err);
+    return false;
+  }
+};
