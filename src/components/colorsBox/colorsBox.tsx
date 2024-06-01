119379c16c6a06c4e30721a445a1569b9c81c404
Balzac <balzac.baudemont@gmail.com>
2024-06-01 23:07:46 +0200

    add to clipboard on click a color

--- src/components/colorsBox/colorsBox.tsx
+++ src/components/colorsBox/colorsBox.tsx
@@ -1,9 +1,12 @@
-import { useSelector } from "react-redux";
-import { join } from "../../constants/helpers";
+import { useDispatch, useSelector } from "react-redux";
+import { generateRandomId, join } from "../../constants/utils";
 import { RootState } from "../../reducers/store";
 import Tooltip from "../tooltip/Tooltip";
+import { addNotification } from "../../reducers/notifications";
+import { copyToClipboard } from "../../constants/utils";
 
 function ColorsBox() {
+  const dispatch = useDispatch();
   const colors = useSelector((state: RootState) => state.colors);
 
   const colorClassName = (index: number, length: number) =>
@@ -13,19 +16,27 @@ function ColorsBox() {
       index === length - 1 ? "rounded-r-lg" : "",
     ]);
 
-  // TODO: add to press-paper on click a color
+  const onClickColor = async (color: string) => {
+    const success = await copyToClipboard(color);
+    const notification = {
+      id: generateRandomId(),
+      title: success ? "Success !" : "Error !",
+      message: success
+        ? `Color ${color} copied to clipboard`
+        : "Failed to copy color, check console",
+    };
+
+    dispatch(addNotification(notification));
+  };
 
   return (
     <div className="flex w-full h-10">
       {Object.values(colors).map((color, index) => (
-        <Tooltip
-          className="flex-1"
-          content={color}
-          key={index}
-        >
-          <div
+        <Tooltip className="flex-1" content={color} key={index}>
+          <button
             className={colorClassName(index, Object.values(colors).length)}
             style={{ backgroundColor: color }}
+            onClick={() => onClickColor(color)}
           />
         </Tooltip>
       ))}
