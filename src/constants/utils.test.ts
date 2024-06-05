c3a0680551cca0205d7d7fd22d899fc084017adb
Balzac <balzac.baudemont@gmail.com>
2024-06-05 10:37:35 +0200

    init jest + adding test for utils methods

---
+++ src/constants/utils.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test } from "@jest/globals";
+import { generateRandomId, join } from "./utils";
+
+describe("Utils: join()", () => {
+  test("Basic behavior, join array of string with space", () => {
+    expect(join(["hello", "world"])).toBe("hello world");
+  });
+
+  test("Doesn't break for an empty array", () => {
+    expect(join([])).toBe("");
+  });
+
+  test("Only one element", () => {
+    expect(join(["hello"])).toBe("hello");
+  });
+
+  test("Empty string", () => {
+    expect(join(["hello", "", "world"])).toBe("hello  world");
+  });
+
+  test("elements contains spaces", () => {
+    expect(join(["hello world", "great world"])).toBe(
+      "hello world great world"
+    );
+  });
+});
+
+describe("Utils: generateRandomId()", () => {
+  test("Basic behavior, generate an id of length 9", () => {
+    expect(generateRandomId()).toHaveLength(9);
+  });
+
+  test("generates different ids on multiple calls", () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(generateRandomId());
+    }
+    // Ensure all generated IDs are unique
+    expect(ids.size).toBe(50);
+  });
+});
+
+
+// Add tests for copyToClipboard()
+// });
