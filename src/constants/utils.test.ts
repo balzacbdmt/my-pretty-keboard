085e9fe04782766dcd62890f48c75d336249c299
Balzac <balzac.baudemont@gmail.com>
2024-06-08 19:03:34 +0200

    use vitest instead of jest

--- src/constants/utils.test.ts
+++ src/constants/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "vitest";
 import { generateRandomId, join } from "./utils";
 
 describe("Utils: join()", () => {
